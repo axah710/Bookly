b052ae7055fbf01d8462db3a4a1dd4b85a466e59
axah710 <ahmed36085361@gmail.com>
2024-04-14 17:19:42 +0200

    Add delayed navigation to HomeView
    Using Future.delayed to delay navigation to HomeView by 2 seconds for a smoother user experience. This change improves the user flow by introducing a delay before transitioning to the HomeView using Get.to.

--- lib/features/splash/presintation/views/widgets/navigate_to_home.dart
+++ lib/features/splash/presintation/views/widgets/navigate_to_home.dart
@@ -3,11 +3,18 @@ import 'package:bookly_app/features/home/presintation/views/home_view.dart';
 import 'package:get/get.dart';
 
 void navigateToHome() {
-    Future.delayed(const Duration(seconds: 2), () {
-      Get.to(
-        () => const HomeView(),
-        transition: Transition.fade,
-        duration: kTranstionDuration,
-      );
-    });
-  }
+  Future.delayed(const Duration(seconds: 2), () {
+    Get.to(
+      () => const HomeView(),
+      transition: Transition.fade,
+      duration: kTranstionDuration,
+    );
+  });
+}
+// Using get state mangament to navigate...
+// The function uses the Future.delayed method to delay the execution of the
+//code inside it by 2 seconds.
+// After the delay, the Get.to method is called to navigate to the HomeView.
+// The HomeView is passed as a callback function to the Get.to method.
+// The transition animation is set to Transition.fade and the duration is
+//set to kTranstionDuration (milliseconds: 256).
