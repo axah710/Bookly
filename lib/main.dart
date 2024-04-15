e2acc648018ef4f37feabb7f07f95d415099e8ba
axah710 <ahmed36085361@gmail.com>
2024-04-15 21:08:19 +0200

    Add Google Fonts package for custom text themes
    Import the Google Fonts package and use it to customize text themes for dark mode. This commit also updates the pubspec.yaml file to include the Google Fonts dependency.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presintation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -21,6 +22,10 @@ class BooklyApp extends StatelessWidget {
         // brightness: Brightness.dark,
       ).copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(
+          ThemeData.dark().textTheme,
+          // To let the font knows that I work on dark mode...
+        ),
       ),
       home: const SplashView(),
     );
