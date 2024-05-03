357d312cce21a43b862c76d19f0c8722b7902038
axah710 <ahmed36085361@gmail.com>
2024-05-03 17:19:33 +0300

    Refactor Epub class for JSON serialization
    Define 'Epub' class with 'fromJson' and 'toJson' methods for JSON serialization.
    Ensure 'isAvailable' property is set correctly in 'fromJson' method.

--- lib/features/home/data/models/book_model/epub.dart
+++ lib/features/home/data/models/book_model/epub.dart
@@ -1,18 +1,39 @@
 import 'package:equatable/equatable.dart';
 
 class Epub extends Equatable {
-	final bool? isAvailable;
+  const Epub({this.isAvailable});
 
-	const Epub({this.isAvailable});
+  factory Epub.fromJson(Map<String, dynamic> json) => Epub(
+        isAvailable: json['isAvailable'] as bool?,
+      );
 
-	factory Epub.fromJson(Map<String, dynamic> json) => Epub(
-				isAvailable: json['isAvailable'] as bool?,
-			);
+  final bool? isAvailable;
 
-	Map<String, dynamic> toJson() => {
-				'isAvailable': isAvailable,
-			};
+  @override
+  List<Object?> get props => [isAvailable];
 
-	@override
-	List<Object?> get props => [isAvailable];
+  Map<String, dynamic> toJson() => {
+        'isAvailable': isAvailable,
+      };
 }
+//It is a class called 'Epub' that extends the 'Equatable' class. It has a 
+//single nullable boolean property called 'isAvailable'. The class has a 
+//constructor that takes an optional parameter 'isAvailable'. It also has
+// a factory method 'fromJson' that takes a JSON object and returns an 
+//instance of 'Epub' with the 'isAvailable' property set based on the 
+//value in the JSON object. The class also has a 'toJson' method that 
+//converts the 'Epub' object to a JSON object. Finally, the class overrides
+// the 'props' getter from the 'Equatable' class to return a list containing 
+//the 'isAvailable' property.
+
+// Flow:
+// The 'Epub' class is defined with a nullable boolean property 'isAvailable'.
+// The class has a constructor that takes an optional parameter 'isAvailable' 
+//and assigns it to the 'isAvailable' property.
+// The class has a factory method 'fromJson' that takes a JSON object and
+// returns an instance of 'Epub' with the 'isAvailable' property set based
+// on the value in the JSON object.
+// The class has a 'toJson' method that converts the 'Epub' object to a 
+//JSON object by creating a map with the 'isAvailable' property.
+// The class overrides the 'props' getter from the 'Equatable' class to 
+//return a list containing the 'isAvailable' property.
