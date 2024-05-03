7a281fa00cabe5296e1b31914e069e9be2a6314c
axah710 <ahmed36085361@gmail.com>
2024-05-03 18:23:23 +0300

    Add abstract class Failure and ServerFailure class, update HomeRepo methods
    - Added an abstract class Failure and a ServerFailure class.
    - Updated the HomeRepo methods fetchBestSellersBooks and fetchFeaturedBooks
      to return a Future that resolves to an Either object for better error handling.

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,26 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  Future<dynamic> fetchBestSellersBooks();
-  Future<dynamic> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellersBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  //   Future<Either<Failure Class,List<TheModel>>>
 }
+// The code is an abstract class called HomeRepo that defines two methods:
+// fetchBestSellersBooks and fetchFeaturedBooks.
+// Both methods return a Future that resolves to an Either object.
+//The Either object can hold either a Failure object or
+//a list of BookModel objects.
+// Either used to create a good error handling mechanism.
+
+// Flow:
+// The fetchBestSellersBooks method is defined with a return type
+//of Future<Either<Failure, List<BookModel>>>.
+//This means that when this method is called, it will return a Future
+// that resolves to an Either object.
+// The Either object can hold either a Failure object or a list of
+// BookModel objects. This allows the method to handle
+//both success and failure cases.
+// The fetchFeaturedBooks method is defined in the same
+//way as fetchBestSellersBooks, with the same return type.
