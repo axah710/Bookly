8ba7167e1fa0f040395bf366e8cd46d4978484de
axah710 <ahmed36085361@gmail.com>
2024-05-03 17:58:16 +0300

    Add Dartz dependency version 0.10.1
    Add Dartz dependency version 0.10.1 to the pubspec.yaml file. This dependency is required for the project.

--- lib/features/home/data/models/book_model/epub.dart
+++ lib/features/home/data/models/book_model/epub.dart
@@ -16,24 +16,24 @@ class Epub extends Equatable {
         'isAvailable': isAvailable,
       };
 }
-//It is a class called 'Epub' that extends the 'Equatable' class. It has a 
-//single nullable boolean property called 'isAvailable'. The class has a 
+//It is a class called 'Epub' that extends the 'Equatable' class. It has a
+//single nullable boolean property called 'isAvailable'. The class has a
 //constructor that takes an optional parameter 'isAvailable'. It also has
-// a factory method 'fromJson' that takes a JSON object and returns an 
-//instance of 'Epub' with the 'isAvailable' property set based on the 
-//value in the JSON object. The class also has a 'toJson' method that 
+// a factory method 'fromJson' that takes a JSON object and returns an
+//instance of 'Epub' with the 'isAvailable' property set based on the
+//value in the JSON object. The class also has a 'toJson' method that
 //converts the 'Epub' object to a JSON object. Finally, the class overrides
-// the 'props' getter from the 'Equatable' class to return a list containing 
+// the 'props' getter from the 'Equatable' class to return a list containing
 //the 'isAvailable' property.
 
 // Flow:
 // The 'Epub' class is defined with a nullable boolean property 'isAvailable'.
-// The class has a constructor that takes an optional parameter 'isAvailable' 
+// The class has a constructor that takes an optional parameter 'isAvailable'
 //and assigns it to the 'isAvailable' property.
 // The class has a factory method 'fromJson' that takes a JSON object and
 // returns an instance of 'Epub' with the 'isAvailable' property set based
 // on the value in the JSON object.
-// The class has a 'toJson' method that converts the 'Epub' object to a 
+// The class has a 'toJson' method that converts the 'Epub' object to a
 //JSON object by creating a map with the 'isAvailable' property.
-// The class overrides the 'props' getter from the 'Equatable' class to 
+// The class overrides the 'props' getter from the 'Equatable' class to
 //return a list containing the 'isAvailable' property.
