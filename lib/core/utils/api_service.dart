09580da7b646d00e93a777ce328a56252c764ad5
axah710 <ahmed36085361@gmail.com>
2024-05-14 21:48:54 +0300

    Update ApiService class constructor parameter to use private Dio instance
    Changed the ApiService class constructor parameter from 'dio' to '_dio'
    to use a private Dio instance for making API requests. This ensures
    encapsulation and better code organization.

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -2,11 +2,11 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   String baseUrl = "https://www.googleapis.com/books/v1/";
-  final Dio dio;
+  final Dio _dio;
 
-  ApiService(this.dio);
+  ApiService(this._dio);
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await dio.get("$baseUrl$endPoint");
+    var response = await _dio.get("$baseUrl$endPoint");
     return response.data;
   }
 }
