0144c736d40d3083e4d5faae144f6608d7c39521
axah710 <ahmed36085361@gmail.com>
2024-05-14 23:43:58 +0300

    Add Dio import and update error handling in fetchBestNewsBooks method
    Import 'package:dio/dio.dart' in HomeRepoImlemintation and update error handling
    in fetchBestNewsBooks method to specifically handle DioException for network
    errors and provide more detailed error information for other exceptions.

--- lib/features/home/data/repos/home_repo_implemintation.dart
+++ lib/features/home/data/repos/home_repo_implemintation.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImlemintation extends HomeRepo {
   ApiService apiService;
@@ -11,8 +12,13 @@ class HomeRepoImlemintation extends HomeRepo {
   );
 
   // This ApiService is used to make Dio requests to an external API.
+  // HomeRepoImlemintation extends HomeRepo and includes an
+  // instance of ApiService to make HTTP requests.
   @override
   Future<Either<Failure, List<BookModel>>> fetchBestNewsBooks() async {
+    // This method fetches books that are free, related to programming,
+    //and sorted by the newest first. It constructs a
+    //specific endpoint using these parameters.
     String sorting = "newest";
     String subject = "programming";
     String programmingBooksEndPoint =
@@ -36,15 +42,27 @@ class HomeRepoImlemintation extends HomeRepo {
       // each representing a book. These items are then transformed
       //into BookModel instances and added to a list.
       return right(books);
-    } catch (e) {
-      return left(
-        ServerFailure(),
-      );
       // If the data fetching and processing are successful,
       // it returns the list of books wrapped in a right()
-      //function (indicating success). If an error occurs,
-      //it catches the exception and returns a left()
-      // containing a ServerFailure object (indicating failure).
+      //function (indicating success).
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      //  If there's an error (like a network issue handled by DioException),
+      // it returns a Failure object wrapped in an Either type
+      // indicating failure (left).
+      // The method handles errors specifically catching DioException
+      //to handle network-related errors and other exceptions for general errors.
+      else {
+        return left(
+          ServerFailure(
+            e.toString(),
+          ),
+        );
+      }
     }
   }
 
