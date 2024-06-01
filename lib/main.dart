55e859c54d7127154ff0190558ada4509be85397
axah710 <ahmed36085361@gmail.com>
2024-06-01 15:12:22 +0300

    Remove commented out code and unused properties in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,14 +49,6 @@ class BooklyApp extends StatelessWidget {
           // textTheme: This defines the text theme for the app. It uses the
           //Montserrat font family and applies it to the dark text theme.
           colorScheme: const ColorScheme.dark(),
-          // iconButtonTheme: const IconButtonThemeData(
-          //   style: ButtonStyle(
-          //     iconColor: MaterialStatePropertyAll(
-          //       Colors.white,
-          //     ),
-          //   ),
-          // ),
-          // brightness: Brightness.dark,
           useMaterial3: true,
           scaffoldBackgroundColor: kPrimaryColor,
           textTheme: GoogleFonts.montserratTextTheme(
